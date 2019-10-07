187da6284d2411c1c7c68904205c3f2b6eba24a9
Grom <gromimol@ukr.net>
2019-10-07 14:26:28 +0300

    Правки

--- js/common.js
+++ js/common.js
@@ -33,9 +33,9 @@ $(document).ready(function () {
 	$('.main-slider').slick({
 		appendArrows: '.main-slider__arrows',
 		dots: true,
+		initialSlide: 1,
 		appendDots: '.main-slider__dots',
 		fade: true,
-		infinite: false,
 		prevArrow: '<span class="prev-arrow"><svg  width="14" height="25" xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:avocode="https://avocode.com/" viewBox="0 0 14 25"><g><g><path d="M1.35331,13.35296l-0.70745,-0.70721l0.14625,-0.14624l-0.14625,-0.14624l0.70745,-0.70721l0.14614,0.14614l11.14676,-11.1461l0.70699,0.70678l-11.14646,11.14662l11.14646,11.14662l-0.70699,0.70678l-11.14676,-11.1461z" fill="currentColor" fill-opacity="1"></path></g></g></svg></span>',
 		nextArrow: '<span class="next-arrow"><svg  width="14" height="25" xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:avocode="https://avocode.com/" viewBox="0 0 14 25"><g><g><path d="M0.6458,1.3529l0.70745,-0.70678l11.14655,11.1463l0.14636,-0.14635l0.70699,0.70721l-0.14624,0.14624l0.14624,0.14624l-0.70699,0.70721l-0.14636,-0.14635l-11.14655,11.1463l-0.70745,-0.70678l11.14688,-11.14662z" fill="currentColor" fill-opacity="1"></path></g></g></svg></span>',
 		responsive: [
@@ -61,6 +61,6 @@ $(document).ready(function () {
         $('.main-slider').slick('slickGoTo', slide);
       });
 
-
+	
 
 })
